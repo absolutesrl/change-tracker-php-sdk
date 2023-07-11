77cdfba2fc36b6d81061b22d67caf86997411db2
Massimo1204 <xiangmassimo@gmail.com>
2023-07-11 14:47:58 +0200

    changetracker changes

--- src/ChangeTracker.php
+++ src/ChangeTracker.php
@@ -7,56 +7,45 @@ use Absolute\ChangeTrackerPhpSdk\Model\Table;
 use function Absolute\ChangeTrackerPhpSdk\Core\generateToken;
 use function Absolute\ChangeTrackerPhpSdk\Core\diff;
 use function Absolute\ChangeTrackerPhpSdk\Core\store;
-use Closure;
 
 class ChangeTracker {
-
-    public Closure $changeTracker;
     public ModelTracker $modelTracker;
-    public Closure $store;
-    public Closure $diff;
-    public Closure $generateToken;
+
     public Row $row;
     public Field $field;
     public Table $table;
 
-    function __construct(Closure $changeTracker, ModelTracker $modelTracker, Closure $store, Closure $diff, Closure $generateToken, Row $row, Field $field, Table $table)
+    function __construct()
     {
-        $this->changeTracker = $changeTracker;
-        $this->modelTracker = $modelTracker;
-        $this->store = $store;
-        $this->diff = $diff;
-        $this->generateToken = $generateToken;
-        $this->row = $row;
-        $this->field = $field;
-        $this->table = $table;
+        $this->modelTracker = new ModelTracker();
+        $this->row = new Row();
+        $this->field = new Field('');
+        $this->table = new Table();
     }
 
-    public static function service() : ChangeTracker {
+    public function changeTracker(...$args) {
 
-        $changeTrackerService = makeChangeTrackerService([
+        $service = makeChangeTrackerService([
             'generateToken' => fn(...$args) => generateToken(...$args),
             'diff' => fn(...$args) => diff(...$args),
             'store' => fn(...$args) => store(...$args),
             'createTable' => fn(...$args) => Table::createTable(...$args)
         ]);
 
-        $store = fn(...$args) => store(...$args);
-        $diff = fn(...$args) => diff(...$args);
-        $generateToken = fn(...$args) => generateToken(...$args);
+        return $service(...$args);
+    }
 
-        return new ChangeTracker($changeTrackerService, new ModelTracker(), $store, $diff, $generateToken, new Row(), new Field(''), new Table());
+    public function store(...$args): ?object
+    {
+        return store(...$args);
+    }
+
+    public function diff(...$args): ?Row
+    {
+        return diff(...$args);
+    }
+    public function generateToken(...$args): string
+    {
+        return generateToken(...$args);
     }
 }
-
-/*
-return [
-    'changeTracker' => $changeTrackerService,
-    'modelTracker' => 'modelTracker',
-    'store' => 'store',
-    'diff' => 'diff',
-    'generateToken' => 'generateToken',
-    'Field' => 'Field',
-    'Row' => 'Row',
-    'Table' => 'Table'
-];*/
